37ac7ac74776b2bec73c8f63cabe105d13834f20
VoKaP <vokaptv@gmail.com>
2025-05-13 20:45:53 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import "./index.module.css"
 
 export default function Home(): JSX.Element {
   useEffect(() => {
-    window.location.href = "https://docs.traffy.site"
+    window.location.href = "https://docs.traffy.site/docs"
   }, [])
   const {siteConfig} = useDocusaurusContext();
   return (
